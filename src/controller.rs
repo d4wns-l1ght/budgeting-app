010803319d88ab9b42347ee7089127673be41168
d4wns-l1ght <neovx.23@proton.me>
2025-08-18 11:52:02 +1200

    feat(controller): add basic key event handling

--- src/controller.rs
+++ src/controller.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use ratatui::crossterm::event::Event;
+use ratatui::crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
 
 use crate::{model::Model, view::View};
 
@@ -25,6 +25,19 @@ pub struct Controller {
 
 impl Controller {
 	pub fn handle_events(&self, event: Event, model: &mut Model, view: &mut View) -> Result<()> {
-		todo!()
+		match event {
+			Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+				self.handle_key_event(key_event, model, view);
+			}
+			_ => {}
+		}
+		Ok(())
+	}
+
+	fn handle_key_event(&self, key_event: KeyEvent, model: &mut Model, view: &mut View) {
+		match key_event.code {
+			KeyCode::Char('q') => model.exit = true,
+			_ => {}
+		}
 	}
 }
